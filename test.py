6de77517dccb0b7482e3b2f514485610498f759b
simran-gopal <154580325+simran-gopal@users.noreply.github.com>
2024-02-02 12:01:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ if len(a)>2:
         data_df = pd.read_sql_query(f'''select occupation from user_setting us left join user u on us.user_id=u.id where email like '%{a}%';''', mynzo_db_read)
         mynzo_db_read.close()
         test.write('Generated Table:')
-        csv_data = display.to_csv(index=False)
+        csv_data = data_df.to_csv(index=False)
         st.download_button(label="Download Table as CSV",data=csv_data,file_name='generated_table.csv',key='download_button')
     else:
         st.write('There is no data for this Email')
