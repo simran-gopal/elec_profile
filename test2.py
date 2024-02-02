49a6463aacaa5611fa7980b8c87dba349e493d28
simran-gopal <154580325+simran-gopal@users.noreply.github.com>
2024-02-02 12:27:00 +0530

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,45 @@
+import subprocess
+import sys
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+if os.path.exists('requirements.txt'):
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+
+import pandas as pd
+import streamlit as st
+import pandas.io.sql as sqlio
+import json
+import geopy.distance
+import mysql.connector
+import configparser
+import reverse_geocoder as rg
+
+config = configparser.ConfigParser()
+config.read(r"mynzo_staging_config.ini")
+
+mynzo_db_read = mysql.connector.connect(host=config['mynzo_db_read']['host'],
+                                   user=config['mynzo_db_read']['user'],
+                                   password=config['mynzo_db_read']['password'],
+                                   database=config['mynzo_db_read']['database'], 
+                                   connection_timeout=int(config['mynzo_db_read']['connection_timeout']))
+
+geo_table = pd.read_sql_query('select id, code, latitude, longitude, parent_id from geo', mynzo_db_read)
+
+st.title('Electric Profile Generator')
+
+a = st.text_input("Enter the Email:")
+if len(a) > 2:
+    if st.button('Generate Table'):
+        data_df = pd.read_sql_query(f'''select occupation from user_setting us left join user u on us.user_id=u.id where email like '%{a}%';''', mynzo_db_read)
+        mynzo_db_read.close()
+
+        if not data_df.empty:
+            st.write("Generated Table:")
+            st.write(data_df)
+        else:
+            st.write('There is no data for this Email')
+else:
+    st.write('Please enter a valid Email')
+
